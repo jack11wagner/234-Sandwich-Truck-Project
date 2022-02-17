9c9748049fd671caead7186492ced738af07f49e
kovacsn <kovacsn@moravian.edu>
2022-02-17 11:08:42 -0500

    Made ROAD_SPACING static

--- src/SimSettings.java
+++ src/SimSettings.java
@@ -1,5 +1,5 @@
 public class SimSettings {
     public static int DIMENSION = 700;
     public static int NUM_ROADS = 10;
-    public int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1);
+    public static int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1);
 }
