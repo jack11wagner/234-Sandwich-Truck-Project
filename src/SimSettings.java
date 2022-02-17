8c006292400cb64b629e4e21f2417e616ae6b1ba
kovacsn <kovacsn@moravian.edu>
2022-02-17 13:01:14 -0500

    Make SimSettings Final

--- src/SimSettings.java
+++ src/SimSettings.java
@@ -1,6 +1,6 @@
 public class SimSettings {
-    public static int DIMENSION = 700;
-    public static int NUM_ROADS = 10;
-    public static int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1);
-    public static int TRUCK_SIZE = DIMENSION / 17;
+    public static final int DIMENSION = 700;
+    public static final int NUM_ROADS = 10;
+    public static final int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1);
+    public static final int TRUCK_SIZE = DIMENSION / 17;
 }
