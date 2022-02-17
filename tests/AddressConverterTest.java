a8faaa5de1de7fd6b377f6684ed0d1b90d05dcd7
Michael Shimer <shimerm@moravian.edu>
2022-02-16 20:45:06 -0500

    added the address converter class still working on the tests

---
+++ tests/AddressConverterTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import java.beans.Transient;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AddressConverterTest {
+    
+    private String order;
+    AddressConverter converter;
+
+    @Test
+    public void testLetterStreetAddress() {
+        converter = new AddressConverter();
+        order = "date, 250 E St., food";
+        System.out.printf(converter.convert(order));
+    }
+
+
+
+}
