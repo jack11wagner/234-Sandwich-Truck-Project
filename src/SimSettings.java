d3942934c38dee8d3f6bbb7229edc87459ae5ec7
kovacsn <kovacsn@moravian.edu>
2022-02-21 16:30:03 -0500

    Move ROAD_WIDTH to SimSettings

--- src/SimSettings.java
+++ src/SimSettings.java
@@ -1,6 +1,7 @@
 public class SimSettings {
     public static final int DIMENSION = 700;
     public static final int NUM_ROADS = 10;
-    public static final int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1);
+    public static final int ROAD_WIDTH = DIMENSION / 35;
+    public static final int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1) - ROAD_WIDTH / (NUM_ROADS + 1);
     public static final int TRUCK_SIZE = DIMENSION / 17;
 }
