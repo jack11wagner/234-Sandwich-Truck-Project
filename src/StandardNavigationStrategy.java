d30a681262851ae100c9c5b5e7d7566090db04c6
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-03-10 10:06:50 -0500

    Rename StandardNavigation to StandardNavigationStrategy

---
+++ src/StandardNavigationStrategy.java
@@ -0,0 +1,10 @@
+public class StandardNavigationStrategy implements NavigationStrategy{
+
+    public StandardNavigationStrategy() {}
+
+    @Override
+    public void moveToDestination() {}
+
+    @Override
+    public void setTruck() {}
+}
