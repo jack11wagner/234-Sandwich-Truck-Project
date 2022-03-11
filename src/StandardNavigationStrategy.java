f9dd4d77761a2e3787a983a138a21a84123601ad
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-03-11 14:26:27 -0500

    Implement calculateNavInstructions in StandardNavigation Strategy

--- src/StandardNavigationStrategy.java
+++ src/StandardNavigationStrategy.java
@@ -1,8 +1,34 @@
+import java.util.Collection;
+import java.util.LinkedList;
+
 /**
  * Author: Nikolas Kovacs
  * This class implements the NavigationStrategy for Standard Navigation behavior
  */
 public class StandardNavigationStrategy extends NavigationStrategy{
 
-    public void calculateNavInstructions(int[] begCoords, int[] destCoords) {}
+    public Collection<int[]> calculateNavInstructions(int[] begCoords, int[] destCoords) {
+        LinkedList<int[]> navigationInstructions = new LinkedList<>();
+        // example element (0,2) move right 2, (1,2) move down two, (0, -2) move left 2, (1, -2) move up 2
+
+        int begX = begCoords[0];
+        int begY = begCoords[1];
+        int destX = destCoords[0];
+        int destY = destCoords[1];
+
+        int xDistance = destX - begX;
+        int yDistance = destY - begY;
+
+        // randomly choose whether to move in the X or Y direction first
+        if ((int) (Math.random() * 1) == 0) {// X direction first
+            navigationInstructions.add(new int[]{0, xDistance});
+            navigationInstructions.add(new int[]{1, yDistance});
+        } else {
+            navigationInstructions.add(new int[]{1, yDistance});
+            navigationInstructions.add(new int[]{0, xDistance});
+        }
+
+
+        return navigationInstructions;
+    }
 }
