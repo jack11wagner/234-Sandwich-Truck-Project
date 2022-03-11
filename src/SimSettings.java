ed72cfa0a2651f990131c27d440a121396ab5221
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-03-11 14:26:00 -0500

    Set cycle method (wait 50 milliseconds)

--- src/SimSettings.java
+++ src/SimSettings.java
@@ -8,9 +8,21 @@ public class SimSettings {
     public static final int ROAD_WIDTH = DIMENSION / 35;
     public static final int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1) - ROAD_WIDTH / (NUM_ROADS + 1);
     public static final int TRUCK_SIZE = DIMENSION / 17;
-    public static final int TRUCK_SPEED = 1; // unit = pixels per cycle
+    public static final int TRUCK_SPEED = 1; // unit = pixels per cycle (defined below
 
     // Truck begins at the intersection of roads E5
     public static final int INITIAL_TRUCK_X = ROAD_SPACING * 4; // Road E
     public static final int INITIAL_TRUCK_Y = ROAD_SPACING * 5; // Road 5
+
+
+    public static void cycle() {
+        try
+        {
+            Thread.sleep(50);
+        }
+        catch(InterruptedException ex)
+        {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
