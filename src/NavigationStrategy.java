a8a242f8cf33daf7c16607ac299803b071aae6d9
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-03-11 13:21:42 -0500

    change moveToDestination to calculateNavInstructions

--- src/NavigationStrategy.java
+++ src/NavigationStrategy.java
@@ -3,15 +3,16 @@
  * This is an abstract class that is meant to be inherited from in order to implement different navigation modes
  */
 public abstract class NavigationStrategy {
-    private Truck truck;
+//    private Truck truck;
+//
+//    /**
+//     * This method is only necessary to give the Strategy the truck, which cannot be done at instantiation time
+//     * Truck MUST call this method in its constructor
+//     * @param truck, given truck so it can control its movements
+//     */
+//    public void setTruck(Truck truck) {
+//        this.truck = truck;
+//    }
 
-    /**
-     * This method is only necessary to give the Strategy the truck, which cannot be done at instantiation time
-     * @param truck, given truck so it can control its movements
-     */
-    public void setTruck(Truck truck) {
-        this.truck = truck;
-    }
-
-    public abstract void moveToDestination(int[] coords);
+    public abstract void calculateNavInstructions(int[] coords);
 }
