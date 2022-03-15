7fa3ff188874b10b8af6830a1a4eb00451173a6e
Jack Wagner <jacksonjw519@gmail.com>
2022-03-15 10:00:07 -0400

    Made number of orders a SimSettings variable

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
     public static void main(String[] args) throws FileFormatException, IOException {
         OrderList ol = new OrderList();
-        ol.generateOrders(100);
+        ol.generateOrders(SimSettings.NUMBER_OF_ORDERS);
 
         Window simWindow = new Window();
 
         Truck truck = new Truck(simWindow, new DistanceBasedStrategy(ol), new StandardNavigationStrategy());
 
         truck.start();
+
     }
 }
--- src/SimSettings.java
+++ src/SimSettings.java
@@ -8,8 +8,9 @@ public class SimSettings {
     public static final int ROAD_WIDTH = DIMENSION / 35;
     public static final int ROAD_SPACING = DIMENSION/(NUM_ROADS - 1) - ROAD_WIDTH / (NUM_ROADS + 1);
     public static final int TRUCK_SIZE = DIMENSION / 17;
-    public static final int TRUCK_SPEED = 1; // unit = pixels per cycle (defined below
+    public static final int TRUCK_SPEED = 1; // unit = pixels per cycle (defined below)
     public static final int PIN_SIZE = DIMENSION / 30;
+    public static final int NUMBER_OF_ORDERS = 100;
 
     // Truck begins at the intersection of roads E5
     public static final int INITIAL_TRUCK_X = ROAD_SPACING * 4; // Road E
