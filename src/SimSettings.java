347170c591448c80d0f51e1383198ca94c1311ae
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-03-23 17:24:33 -0400

    Add NUM_HOUSES_PER_BLOCK setting

--- src/SimSettings.java
+++ src/SimSettings.java
@@ -12,9 +12,10 @@ public class SimSettings {
     public static final int SIM_SPEED = 10; // unit = milliseconds: how often the cycle repeats (defined below)
     public static final int PIN_SIZE = DIMENSION / 25;
     public static final int NUMBER_OF_ORDERS = 100;
-    public static final String SERVICE_CENTER = "550 E St.";
+    public static final int NUM_HOUSES_PER_BLOCK = 10;
+    public static final String SERVICE_CENTER = "550 5 St.";
 
-    private static final int[] initial_truck_xy = new AddressConverter().convert("550 5 St.");
+    private static final int[] initial_truck_xy = new AddressConverter().convert(SERVICE_CENTER);
     public static final int INITIAL_TRUCK_X = initial_truck_xy[0];
     public static final int INITIAL_TRUCK_Y = initial_truck_xy[1];
 
