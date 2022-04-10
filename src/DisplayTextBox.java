aee5cb04567cf6b56efb2c9ce9ca1b1584a8fbe9
Nikolas Kovacs <78886981+nikovacs@users.noreply.github.com>
2022-04-10 18:02:26 -0400

    Class used to display information at top of screen

--- src/DisplayTextBox.java
+++ src/DisplayTextBox.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class DisplayTextBox {
+import javax.swing.*;
+import java.awt.*;
+
+public class DisplayTextBox extends JPanel {
+    String text = "";
+
+    public DisplayTextBox() {
+        super();
+        this.setPreferredSize(new Dimension(SimSettings.DIMENSION, 15));
+    }
+
+    public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setPaint(Color.BLACK);
+        g2.setFont(new Font("Arial", Font.PLAIN, 11));
+        g2.drawString(text, this.getWidth()/2 - text.length()*3, this.getHeight());
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
